63af3d1e5ccab2a839ce58a4664bf432100b0dc8
idrk1618 <126374482+idrk1618@users.noreply.github.com>
2025-02-11 19:40:20 +0300

    32

--- main.py
+++ main.py
@@ -1,14 +1,12 @@
-from fastapi import FastAPI, Request, Response, Form, Depends
+from fastapi import FastAPI, Form
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import HTMLResponse, FileResponse
-from fastapi.templating import Jinja2Templates
-from django.shortcuts import render
-from typing import Optional
 
 import requests
 import json
 import os
-from urllib.request import urlopen
+
+from encrypt import xor_encrypt_decrypt
 
 root = os.path.dirname(os.path.abspath(__file__))
 
@@ -24,10 +22,9 @@ material_data = json.loads(data)
 
 app = FastAPI()
 
-templates = Jinja2Templates(directory="site")
-
 # Говорим АПИ где находятся статик файлы
 app.mount('/static', StaticFiles(directory='static'), 'static')
+
 
 @app.get("/")
 def root():
@@ -44,23 +41,13 @@ def postdata(login = Form(), password=Form()):
 def root():
     return FileResponse("site/tables.html")
 
-# @app.get("/materials")
-#     return FileResponse("site/materials.html")
-# async def get_files_html(request: Request, files=Depends(material_data)):
-#     return templates.TemplateResponse(request=Request, name='materials.html', context={'files': files})
-
-# link_template = "/materials/{file}"
-# @app.post(link_template.format(file=material_data[0]["filename"]))
-# def download():
-#     download_template = "files/materials/{file}"
-#     return FileResponse(download_template.format(file=material_data[0]["filename"]))
-
 @app.get("/materials", response_class=HTMLResponse)
 def root():
     data = "<h1>Материалы</h1>"
     for i in material_data:
-        data = data + f"<p>{i["filename"]}</p>"
+        file = i["filename"]
+        data = data + f'<a href="https://github.com/infgotoinf/School-Site/raw/refs/heads/main/files/materials/{file}">{file}</a>'
+        data = data + f'<button type="submit" id="{file}" class="send">Отправить</button><br>'
     return data
 
-link_template = "/materials/{file}"
-#print(link_template.format(file=material_data[0]["filename"]))
+for js in user_data:
