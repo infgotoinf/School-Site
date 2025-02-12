aed65e1e8fb8779a97adae42ea020ee14e51b4b5
idrk1618 <126374482+idrk1618@users.noreply.github.com>
2025-02-12 07:48:37 +0300

    f

--- main.py
+++ main.py
@@ -84,7 +84,7 @@ def add():
             os.system(f'git commit -m "{filename}"')
             os.system("git push")
 
-    return '<meta http-equiv="refresh" content="0"; action="materials">'
+    return '<meta content="0"; action="materials">'
 
 
 # for js in user_data:
